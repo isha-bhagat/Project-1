7e09439946d22c1efd36fa248ed5b61850978283
isha-bhagat <145487606+isha-bhagat@users.noreply.github.com>
2024-08-23 23:49:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript to handle dropdown toggle and close when clicking outside
+function toggleDropdown() {
+    document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
